19171f600895a181f77a0202fc7bafdf726b177f
darkship <crumble_tk@hotmail.com>
2017-11-15 22:09:19 +0100

    remove useless namespace

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,34 @@
-pub mod transport {
-    pub trait Endpoint{
-        fn read(&self);
-        fn write(&self);
-    }
 
-    pub struct Transport<F: Endpoint, T: Endpoint> {
-        from: F,
-        to: T,
-    }
+pub trait Endpoint{
+    fn read(&self);
+    fn write(&self);
+}
 
-    impl <F: Endpoint, T: Endpoint> Transport<F, T> {
-        pub fn forward (&self) -> Result<bool, &str>{
-            self.from.read();
-            self.to.write();
-            Ok(true)
-        }
-        pub fn backward (&self) -> Result<bool, &str>{
-            self.to.read();
-            self.from.write();
-            Ok(true)
-        }
-    }
+pub struct Transport<F: Endpoint, T: Endpoint> {
+    from: F,
+    to: T,
+}
 
-    pub fn new<F: Endpoint, T: Endpoint>(from: F, to: T) -> Transport<F,T>{
-        Transport{
-            from: from,
-            to: to
-        }
+impl <F: Endpoint, T: Endpoint> Transport<F, T> {
+    pub fn forward (&self) -> Result<bool, &str>{
+        self.from.read();
+        self.to.write();
+        Ok(true)
+    }
+    pub fn backward (&self) -> Result<bool, &str>{
+        self.to.read();
+        self.from.write();
+        Ok(true)
     }
 }
+
+pub fn new<F: Endpoint, T: Endpoint>(from: F, to: T) -> Transport<F,T>{
+    Transport{
+        from: from,
+        to: to
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -36,7 +36,7 @@ mod tests {
     struct TestEndPoint{
 
     }
-    impl transport::Endpoint for TestEndPoint{
+    impl Endpoint for TestEndPoint{
         fn read(&self){}
         fn write(&self){}
     }
@@ -44,7 +44,7 @@ mod tests {
     fn it_works() {
         let start = TestEndPoint{};
         let from = TestEndPoint{};
-        let t = transport::new(start, from);
+        let t = new(start, from);
         assert_eq!(t.forward(), Ok(true));
         assert_eq!(t.backward(), Ok(true));
     }
