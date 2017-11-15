6996702ecc6e025c627dce0011f27cf79bd13e8a
Thomas Kraebihl <thomas.kraebihl@sophos.com>
2017-11-15 16:48:57 +0100

    added endpoint trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,50 @@
-
 pub mod transport {
-    pub struct Transport {
-        
+    pub trait Endpoint{
+        fn read(&self);
+        fn write(&self);
     }
 
-    impl Transport {
+    pub struct Transport<F: Endpoint, T: Endpoint> {
+        from: F,
+        to: T,
+    }
+
+    impl <F: Endpoint, T: Endpoint> Transport<F, T> {
         pub fn forward (&self) -> Result<bool, &str>{
+            self.from.read();
+            self.to.write();
             Ok(true)
         }
         pub fn backward (&self) -> Result<bool, &str>{
+            self.to.read();
+            self.from.write();
             Ok(true)
         }
     }
 
-    pub fn new() -> Transport{
-        Transport{}
+    pub fn new<F: Endpoint, T: Endpoint>(from: F, to: T) -> Transport<F,T>{
+        Transport{
+            from: from,
+            to: to
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+    struct TestEndPoint{
+
+    }
+    impl transport::Endpoint for TestEndPoint{
+        fn read(&self){}
+        fn write(&self){}
+    }
     #[test]
     fn it_works() {
-        let t = transport::new();
+        let start = TestEndPoint{};
+        let from = TestEndPoint{};
+        let t = transport::new(start, from);
         assert_eq!(t.forward(), Ok(true));
         assert_eq!(t.backward(), Ok(true));
     }
